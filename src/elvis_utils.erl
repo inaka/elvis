564e317f92441e7f5fa20d7a19175e3a87eedf92
Андрей Веселов <github.coffee@hotmail.com>
2015-08-26 17:39:12 +0300

    Fix @doc typo

--- src/elvis_utils.erl
+++ src/elvis_utils.erl
@@ -94,7 +94,7 @@ check_nodes([Node | Nodes], Fun, Args, Results) ->
             check_nodes(Nodes, Fun, Args, [Result | Results])
     end.
 
-%% @doc This is defined so tht it an be mocked for tests.
+%% @doc This is defined so that it can be mocked for tests.
 -spec erlang_halt(integer()) -> no_return().
 erlang_halt(Code) ->
     halt(Code).
