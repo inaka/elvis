7d0b8b80cd8cd9c0a580d968e2e82e647fe273c6
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2025-07-17 03:16:59 +0100

    Adapt to new API

--- test/examples/user_defined_rules.erl
+++ test/examples/user_defined_rules.erl
@@ -1,9 +1,9 @@
 -module(user_defined_rules).
 
--export([rule/3]).
+-export([rule/2]).
 
 -hank([unnecessary_function_arguments]).
 
--spec rule(any(), any(), any()) -> [elvis_result:item(), ...].
-rule(_Config, _Target, _) ->
+-spec rule(any(), any()) -> [elvis_result:item(), ...].
+rule(_Config, _Target) ->
     [elvis_result:new(item, "This will always FAIL.", [], 1)].
