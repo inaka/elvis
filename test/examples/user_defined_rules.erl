07063ba2a9f992ddbd1cbece5967cda50b825805
Brujo Benavides <fernando_benavides_rodriguez@baxter.com>
2026-02-11 12:13:51 +0100

    Upgrade elvis_core to 4.2.0 (#597)

--- test/examples/user_defined_rules.erl
+++ test/examples/user_defined_rules.erl
@@ -1,9 +1,9 @@
 -module(user_defined_rules).
 
--export([rule/3]).
+-export([rule/2]).
 
 -hank([unnecessary_function_arguments]).
 
--spec rule(any(), any(), any()) -> [elvis_result:item(), ...].
-rule(_Config, _Target, _) ->
-    [elvis_result:new(item, "This will always FAIL.", [], 1)].
+-spec rule(any(), any()) -> [elvis_result:item(), ...].
+rule(_Rule, _Config) ->
+    [elvis_result:new(item, "This will always FAIL.", [])].
