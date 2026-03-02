fb4ef8d6aa6c5b25aab79145d2d2e5faf392318d
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2026-03-02 21:18:37 +0000

    Adapt to new `elvis_core` configuration validation (#596)

--- test/examples/user_defined_rules.erl
+++ test/examples/user_defined_rules.erl
@@ -5,5 +5,5 @@
 -hank([unnecessary_function_arguments]).
 
 -spec rule(any(), any()) -> [elvis_result:item(), ...].
-rule(_Rule, _Config) ->
+rule(_Config, _Target) ->
     [elvis_result:new(item, "This will always FAIL.", [])].
